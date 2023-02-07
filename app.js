5546011c2614cdd203b25e168ac847534b3d4423
cristianmino <cristianeisasa@gmail.com>
2023-02-07 16:27:21 -0300

    Actualizando la resolución del ejercicio

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 var app = express();
 
 app.use('/', function(req, res){
-    res.send("HcUy6Re2LLBRtj")
+    res.send('<h1>HcUy6Re2LLBRtj</h1>')
 });
 
 app.listen(3000);
